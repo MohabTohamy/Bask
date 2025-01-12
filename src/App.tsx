68857d2c40bb87d0e126cbab1aed3ebf8e94ecab
MohabTohamy <MohabTohamyAbdallah@gmail.com>
2025-01-12 21:20:48 +0200

    first

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,23 @@ import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
 import './index.css';
 import './App.css';
 import Layout from "./Components/Layout/Layout";
-import NotFound from "./Components/NotFound/NotFound";
-import Dashboard from "./Components/Appointment/Dashboard/Dashboard";
-import BasicCalender from "./Components/ReactBigCalender/BasicCalender";
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Charts from "./Components/Charts/Charts";
 
 
 
 
 
 const router = createBrowserRouter([{
-  path: "/", element: <Layout />, children: [{
-    path: "/Calendar",
-    element: <BasicCalender />,
-  },
-  {
-    path: "/appointment",
-    element: <Dashboard />,
-  }, {
-    path: "/*",
-    element: <NotFound />,
-  },]
+  path: "/", element: <Layout />, children: [
+    {
+      path: "/Dashboard",
+      element: <Dashboard />,
+    },
+    {
+      path: "/charts",
+      element: <Charts />,
+    },]
 }
 
 ]);
